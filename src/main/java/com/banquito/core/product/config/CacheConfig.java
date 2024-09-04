6c2ebd6413fa141f4fdd9f13c07926feba01417d
arsanchez5 <138083238+arsanchez5@users.noreply.github.com>
2024-09-04 00:23:31 -0500

    ADD Configuration cache

---
+++ src/main/java/com/banquito/core/product/config/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.banquito.core.product.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
+        factoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        return factoryBean;
+    }
+
+    @Bean
+    public EhCacheCacheManager cacheManager() {
+        return new EhCacheCacheManager(ehCacheManagerFactoryBean().getObject());
+    }
+}
